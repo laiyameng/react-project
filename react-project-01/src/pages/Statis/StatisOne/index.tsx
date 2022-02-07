0fb36f94f7751a016578f29c5b2636b5b9d1dd82
timo <410379528@qq.com>
2022-02-07 17:02:51 +0800

    统计页面初始化

---
+++ react-project-01/src/pages/Statis/StatisOne/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+interface Props {
+    
+}
+ 
+interface State {
+    
+}
+ 
+class  StatisOne extends React.Component<Props, State> {
+    render() { 
+        return ( <div>this is StatisOne Page!</div> );
+    }
+}
+ 
+export default StatisOne;
---
+++ react-project-01/src/pages/Statis/StatisTwo/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+interface Props {
+    
+}
+ 
+interface State {
+    
+}
+ 
+class  StatisTwo extends React.Component<Props, State> {
+    render() { 
+        return ( <div>this is StatisTwo Page!</div> );
+    }
+}
+ 
+export default StatisTwo;
